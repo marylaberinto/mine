60a2620c15c003621b0a8b3fa9870f4ca9766efa
findly <findly@FINDLY-PC>
2016-05-31 11:11:28 +1200

    Add project files.

---
+++ analytics.e2e.testing/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace analytics.e2e.testing
+{
+    public class Class1
+    {
+    }
+}
