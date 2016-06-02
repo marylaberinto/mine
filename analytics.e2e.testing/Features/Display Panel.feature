0e348b654296955a93bf87c674e742b7b8df258a
Mary Laberibto <Mary Laberibto>
2016-06-02 14:10:38 +1200

    Added Display Panel scenario

---
+++ analytics.e2e.testing/Features/Display Panel.feature
@@ -0,0 +1,19 @@
+﻿Feature: Display Panel
+	In order to view the demographics
+	As a CRM user
+	I want to be able to see all the panels
+
+@mytag
+Scenario Outline: Display Panel
+	Given AnalyticsPortalAccess is Feature Toggled On
+	And I log into CRM 
+	When I navigate to the portal
+	Then I should be able to see the analytics page 
+	And I should be able to see the <panels>
+
+	Examples: Display Panels
+	| panels           |
+	| Types            |
+	| Systems          |
+	| Campaigns        |
+	| Profile Statuses |
---
+++ analytics.e2e.testing/StepDefinitions/DisplayPanelSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace findly.TestAutomation.Analytics.StepDefinitions
+{
+    [Binding]
+    public class DisplayPanelSteps
+    {
+        [Given(@"I log into CRM")]
+        public void GivenILogIntoCRM()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should be able to see the Types")]
+        public void ThenIShouldBeAbleToSeeTheTypes()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
