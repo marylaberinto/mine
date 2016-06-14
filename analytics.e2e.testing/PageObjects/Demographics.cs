fb23945dfda65758b3b2411185121c703c5f30e8
Suryatapa Chakravarty <suryatapa.chakravarty@findly.com>
2016-06-14 16:01:54 +1200

    Added bindings for Filter Types tests

--- analytics.e2e.testing/PageObjects/Demographics.cs
+++ analytics.e2e.testing/PageObjects/Demographics.cs
@@ -8,26 +8,54 @@ namespace findly.TestAutomation.Analytics.PageObjects
     public class Demographics
     {
         private readonly BrowserSession _browser = FeatureContextWrapper.BrowserSession;
+        private readonly ElementScope _analyticsiFrame;
+
+        //Analytics Constructor
+        public Demographics()
+        {
+            //In absence of any other attribute, using empty string for finding the frame on the page.
+            _analyticsiFrame = _browser.FindWindow("Findly Analytics").FindFrame("");
+        }
 
         public void AssertDiscoveryuiLoaded()
         {
             Assert.True(_browser.FindId("search").Exists(), "Discovery UI Search bar failed to load");
         }
 
-        public void AssertHasPanelLoaded (List<string> panelNameList)
+        public void AssertHasPanelLoaded(List<string> panelNameList)
         {
-            //In absence of any other attribute, using empty string for finding the frame on the page. 
-            var analyticsiFrame = _browser.FindWindow("Findly Analytics").FindFrame("");
-
-            if (analyticsiFrame.Exists(CoypuOptions.Timeout(60)))
+            if (_analyticsiFrame.Exists(CoypuOptions.Timeout(60)))
             {
                 var panelTitles = new List<string>();
-                var panelElements = analyticsiFrame.FindAllCss(".card__header-title", null, CoypuOptions.Timeout(60));
+                var panelElements = _analyticsiFrame.FindAllCss(".card__header-title", null, CoypuOptions.Timeout(60));
                 foreach (var panelElement in panelElements)
                 {
                     panelTitles.Add(panelElement.Text);
                 }
-                CollectionAssert.AreEquivalent(panelNameList, panelTitles, "Atleast one of the panel titles has not loaded correctly");
+                CollectionAssert.AreEquivalent(panelNameList, panelTitles,
+                    "Atleast one of the panel titles has not loaded correctly");
+            }
+        }
+
+        public void EnterCriteriaInSearchField(string value)
+        {
+            if (_analyticsiFrame.Exists(CoypuOptions.Timeout(60)))
+            {
+                _analyticsiFrame.FindId("search", CoypuOptions.Timeout(20)).FillInWith(value);
+            }
+        }
+
+        public void AssertAllFiltersExist(List<string> filterList)
+        {
+            if (_analyticsiFrame.Exists(CoypuOptions.Timeout(60)))
+            {
+                var actualFilters = new List<string>();
+                var filterElements = _analyticsiFrame.FindAllCss(".square", null, CoypuOptions.Timeout(60));
+                foreach (var filterElement in filterElements)
+                {
+                    actualFilters.Add(filterElement.Text);
+                }
+                CollectionAssert.AreEquivalent(filterList, actualFilters, "Atleast one fo the filters has not loaded correctly");
             }
         }
     }
